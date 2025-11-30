beaefb65deabcaf6e95ba7abf90b1e6fef446f7f
gaylebabacm <goalsightfootball@gmail.com>
2025-11-30 09:44:16 +0530

    Update a_bot.py

--- a_bot.py
+++ a_bot.py
@@ -1,20 +1,29 @@
-from aiogram import Bot, Dispatcher, types
-import asyncio
+# ------------------ IMPORTS ------------------
+from flask import Flask
+from pyrogram import Client, filters
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+# ------------------ FLASK SERVER ------------------
+app = Flask(__name__)
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+@app.route('/')
+def home():
+    return "A Bot is Running 🚀"
 
-@dp.message()
-async def start_handler(message: types.Message):
-    if message.text == "/start":
-        await message.reply("📩 Send Bot API Token")
+# ------------------ TELEGRAM BOT ------------------
+API_ID = 38934704
+API_HASH = "77bf14764bacdbf309aa0d1d786d97d7"
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Environment Variable se token le raha hai
 
-async def main():
-    print("Telegram Bot Started Successfully ✔️")
-    await dp.start_polling(bot)
+bot = Client("A_BOT", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+# ----- START COMMAND -----
+@bot.on_message(filters.command("start"))
+async def start_handler(client, message):
+    await message.reply("📩 Send Bot API Token")
+
+# ------------------ RUN BOTH ------------------
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot.start()
+    port = int(os.environ.get("PORT", 5000))  # PORT Render automatic set karega
+    app.run(host='0.0.0.0', port=port)
