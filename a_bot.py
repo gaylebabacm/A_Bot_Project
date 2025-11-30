b42839d94e82d47ef7bddf4603ee0fc3aa5d2697
gaylebabacm <goalsightfootball@gmail.com>
2025-11-30 10:29:29 +0530

    Update a_bot.py

--- a_bot.py
+++ a_bot.py
@@ -3,6 +3,7 @@ from flask import Flask
 from pyrogram import Client, filters
 import os
 import threading
+import asyncio  # ADD THIS
 
 # ------------------  FLASK SERVER  ------------------
 app = Flask(__name__)
@@ -14,7 +15,7 @@ def home():
 # ------------------  TELEGRAM BOT  ------------------
 API_ID = 38934704
 API_HASH = "77bf14764bacdbf309aa0d1d786d97d7"
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Render se le raha hai
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Client(
     "A_BOT",
@@ -28,12 +29,12 @@ bot = Client(
 async def start_handler(client, message):
     await message.reply("📨 Send Bot API Token")
 
-# ---- BOT KO BACKGROUND ME CHALAO ----
+# ---- BOT KO BACKGROUND THREAD ME RUN KARO ----
 def start_bot():
-    bot.run()
+    asyncio.run(bot.run())   # <-- Fix for ERROR
 
-# ------------------  RUN BOTH  ------------------
+# ------------------  RUN BOTH ------------------
 if __name__ == "__main__":
-    threading.Thread(target=start_bot).start()   # Bot background me run hoga
+    threading.Thread(target=start_bot).start()  # Bot background me run hoga
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
