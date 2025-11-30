e293989776b23e22ebdb776eafbb76dbbfad8ec6
gaylebabacm <goalsightfootball@gmail.com>
2025-11-30 09:26:15 +0530

    Update a_bot.py

--- a_bot.py
+++ a_bot.py
@@ -1,34 +1,20 @@
-from flask import Flask
-from pyrogram import Client, filters
+from aiogram import Bot, Dispatcher, types
+import asyncio
 import os
 
-# Flask Server
-app = Flask(__name__)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-@app.route('/')
-def home():
-    return "Worker Running Successfully! 🔥"
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
-# Telegram Bot Credentials from Environment Variables
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+@dp.message()
+async def start_handler(message: types.Message):
+    if message.text == "/start":
+        await message.reply("📩 Send Bot API Token")
 
-# Telegram Bot Client
-bot = Client(
-    "A_BOT",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
-
-# START Command
-@bot.on_message(filters.command("start"))
-async def start_handler(client, message):
-    await message.reply("[ Send bot api token ]")
-
-# Run Both
-if __name__ == "__main__":
-    bot.start()
+async def main():
     print("Telegram Bot Started Successfully ✔️")
-    app.run(host='0.0.0.0', port=10000)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
